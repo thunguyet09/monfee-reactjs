8fe49294e7df2a039d8bc05d7cc8ca84c5181ab8
thunguyet09 <thnguyet03@gmail.com>
2024-04-26 00:16:51 +0700

    26/4

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect, useRef, useContext } from 'react'
 import styles from './Product.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFilter } from '@fortawesome/free-solid-svg-icons';
+import { getAllProducts } from '../../api';
+
 const Product = () => {
     useEffect(() => {
         const slick_track = document.querySelector(`.${styles.slick_track}`);
@@ -53,11 +55,128 @@ const Product = () => {
           const slideWidth = (slick_track.clientWidth - 400);
           slick_track.style.transform = `translateX(-${slideIndex * slideWidth}px)`;
         }
+
+        const change_column = document.querySelector(`.${styles.change_column}`)
+        const prod_per = document.querySelector(`.${styles.prod_per}`)
+        change_column.addEventListener('mouseenter', () => {
+            prod_per.style.opacity = 1
+            prod_per.style.visibility = 'visible'
+        })
+
+        change_column.addEventListener('mouseleave', () => {
+            prod_per.style.opacity = 0
+            prod_per.style.visibility = 'hidden'
+        })
+
+        const featuredBtn = document.querySelector(`.${styles.featuredBtn}`)
+        const dropdown = document.querySelector(`.${styles.dropdown}`)
+        const featured_icon = document.querySelector(`.${styles.featuredBtn} > span`)
+        let flag = false;
+        featuredBtn.addEventListener('click', () => {
+            flag = !flag
+            if(flag){
+                dropdown.style.opacity = 1
+                featured_icon.innerHTML = `<span class="material-symbols-outlined">arrow_drop_up</span>`
+            }else{
+                dropdown.style.opacity = 0
+                featured_icon.innerHTML = `<span class="material-symbols-outlined">arrow_drop_down</span>`
+            }
+        })
+
         return () => {
           slick_track.removeEventListener('mouseenter', handleMouseEnter);
           slick_track.removeEventListener('mouseleave', handleMouseLeave);
         };
     }, []);
+
+    useEffect(() => {
+        const colors = async () => {
+            const products = await getAllProducts()
+            const uniqueColors = products.reduce((colorSet, item) => {
+                item.colors.forEach(color => colorSet.add(color));
+                return colorSet;
+            }, new Set())
+            const list_color = document.querySelector(`.${styles.list_color}`)
+            list_color.innerHTML = ''
+            uniqueColors.forEach((item) => {
+                const color_item = document.createElement('li')
+                list_color.appendChild(color_item)
+                const color_item_a = document.createElement('a')
+                color_item.appendChild(color_item_a)
+                color_item_a.style.backgroundColor = item
+                color_item_a.title = item
+            })
+        }
+
+        const showProductGrid = async () => {
+            const products = await getAllProducts()
+            const product_items = document.querySelector(`.${styles.product_grid_parent}`)
+            products.forEach((item) => {
+                const row = document.createElement('div')
+                row.className = styles.row
+                product_items.appendChild(row)
+                const img_product = document.createElement('div')
+                row.appendChild(img_product)
+                const img_link = document.createElement('img')
+                img_link.width = 280
+                img_link.src = `./img/${item.img_url[0]}`
+                img_product.appendChild(img_link)
+                const product_icon_action = document.createElement('ul')
+                img_product.appendChild(product_icon_action)
+                if(item.colors){
+                    const select_option = document.createElement('button')
+                    select_option.className = styles.select_option
+                    select_option.innerHTML = `<span class="material-symbols-outlined">more_horiz</span>`
+                    product_icon_action.appendChild(select_option)
+                }else{
+                    const addToCart = document.createElement('button')
+                    addToCart.className = styles.addToCart
+                    addToCart.innerHTML = `<span class="material-symbols-outlined">local_mall</span>`
+                    product_icon_action.appendChild(addToCart)
+                }
+                const quick_view = document.createElement('button')
+                quick_view.innerHTML = `<span class="material-symbols-outlined">search</span>`
+                product_icon_action.appendChild(quick_view)
+
+                const wishlist = document.createElement('button')
+                wishlist.innerHTML = `<span class="material-symbols-outlined">favorite</span>`
+                product_icon_action.appendChild(wishlist)
+
+                const title_product = document.createElement('h4')
+                title_product.className = styles.title_product
+                title_product.textContent = item.name
+                row.appendChild(title_product)
+                if(item.promo_price){
+                    const price_box = document.createElement('div')
+                    row.appendChild(price_box)
+                    const price = document.createElement('h5')
+                    price.innerHTML = `<del>${item.price.toLocaleString()}</del>`
+                    price_box.appendChild(price)
+                    const promo_price = document.createElement('h4')
+                    promo_price.textContent = item.promo_price.toLocaleString()
+                    price_box.appendChild(promo_price)
+                }else{
+                    const price = document.createElement('h4')
+                    price.innerHTML = `${item.price.toLocaleString()}`
+                    row.appendChild(price)
+                }
+
+                if(item.colors.length > 0){
+                    const product_colors = document.createElement('div')
+                    row.appendChild(product_colors)
+                    item.colors.forEach((val) => {
+                        const color_btn = document.createElement('button')
+                        color_btn.style.padding = '10px'
+                        color_btn.style.backgroundColor = val
+                        product_colors.appendChild(color_btn)
+                    })
+                }
+            })
+        }
+
+        showProductGrid()
+        colors()
+    }, [])
     return (
         <>
             <div id={styles.products}>
@@ -107,29 +226,105 @@ const Product = () => {
                 </div>
 
                 <div className={styles.shop_container}>
-                    <div className={styles.widget}></div>
-                    <div className={styles.shop_control}>
-                        <div className={styles.shop_control_col1}>
-                            <button className={styles.filterBtn}>
-                                <span><FontAwesomeIcon icon={faFilter} /></span>
-                                <h3>FILTER</h3>
-                            </button>
+                    <div className={styles.widget}>
+                        <div className={styles.filter_category}>
+                            <div className={styles.widget_title}>
+                                <h3>Categories</h3>
+                            </div>
+                            <div className={styles.widget_content}>
+                                <ul>
+                                    <li><a href=''>Home</a></li>
+                                    <li><a href=''>Shop</a></li>
+                                    <li><a href=''>Featured</a></li>
+                                    <li><a href=''>Pages</a></li>
+                                    <li><a href=''>Element</a></li>
+                                </ul>
+                            </div>
                         </div>
-                        <div className={styles.shop_control_col2}>
-                            <div className={styles.change_column}>
-                                <div className={styles.prod_per}></div>
-                                <button className={styles.icon_change}>
-                                    <img src="./img/column.png" width="35px" height="35px"/>
+                        <div className={styles.color_option}>
+                            <div className={styles.widget_title}>
+                                <h3>Color Option</h3>
+                            </div>
+                            <div>
+                                <ul className={styles.list_color}>
+                                    
+                                </ul>
+                            </div>
+                        </div>
+                        <div className={styles.size_option}>
+                            <div className={styles.widget_title}>
+                                <h3>Size Option</h3>
+                            </div>
+                            <div>
+                                <ul className={styles.list_size}>
+                                    <li><a title="500g">500g</a></li>
+                                    <li><a title="1000g">1000g</a></li>
+                                    <li><a title="1500g">1500g</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className={styles.price_filter_option}>
+                            <div className={styles.widget_title}>
+                                <h3>Price Filter</h3>
+                            </div>
+                            <div>
+                                <ul className={styles.list_price}>
+                                    <li><a>50,000 - 100,000</a></li>
+                                    <li><a>100,000 - 150,000</a></li>
+                                    <li><a>150,000 - 200,000</a></li>
+                                    <li><a>200,000 - 250,000</a></li>
+                                    <li><a>Over 250,000</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className={styles.banner_sidebar}>
+                            <a>
+                                <img src='./img/banner_sidebar.webp' width="350px" />
+                            </a>
+                        </div>
+                    </div>
+                    <div className={styles.product_view}>
+                        <div className={styles.shop_control}>
+                            <div className={styles.shop_control_col1}>
+                                <button className={styles.filterBtn}>
+                                    <span><FontAwesomeIcon icon={faFilter} /></span>
+                                    <h3>FILTER</h3>
                                 </button>
                             </div>
-                            <div className={styles.featured}>
-                                <button className={styles.featuredBtn}>
-                                    <h3>Featured</h3>
-                                    <span class="material-symbols-outlined">arrow_drop_down</span>
-                                </button>
-                                <ul className={styles.dropdown}>
-
-                                </ul>
+                            <div className={styles.shop_control_col2}>
+                                <div className={styles.change_column}>
+                                    <div className={styles.prod_per}>
+                                        <button className={styles.col_size_2}>2</button>
+                                        <button className={styles.col_size_3}>3</button>
+                                        <button className={styles.col_size_4} id={styles.size_active}>4</button>
+                                        <button className={styles.col_size_5}>5</button>
+                                    </div>
+                                    <button className={styles.icon_change}>
+                                        <img src="./img/column.png" width="35px" height="35px"/>
+                                    </button>
+                                </div>
+                                <div className={styles.featured}>
+                                    <button className={styles.featuredBtn}>
+                                        <h3>Featured</h3>
+                                        <span class="material-symbols-outlined">arrow_drop_down</span>
+                                    </button>
+                                    <ul className={styles.dropdown}>
+                                        <li>Featured</li>
+                                        <li>Best Selling</li>
+                                        <li>Alphabetically, A-Z</li>
+                                        <li>Price, high to low</li>
+                                        <li>Price, low to high</li>
+                                        <li>Date, old to new</li>
+                                        <li>Date, new to old</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles.product_grid_uniform}>
+                            <div className={styles.product_items}>
+                                <div className={styles.product_item_v1}>
+                                    <div className={styles.product_grid_parent}></div>
+                                </div> 
                             </div>
                         </div>
                     </div>
