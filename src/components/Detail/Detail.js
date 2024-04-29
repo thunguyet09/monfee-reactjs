c53af511fd7f8118b012d54b6343af5a3ef08bda
thunguyet09 <thnguyet03@gmail.com>
2024-04-29 23:57:38 +0700

    29/4

--- src/components/Detail/Detail.js
+++ src/components/Detail/Detail.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import styles from './Detail.module.css'
-import { getAllProducts, getDetail } from '../../api';
+import { getAllProducts, getDetail, getProductsByCategoryId } from '../../api';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-regular-svg-icons';
 const Detail = () => {
@@ -126,6 +126,7 @@ const Detail = () => {
                     prices.appendChild(promo_price)
                 }else{
                     const price = document.createElement('h2')
+                    price.className = styles.price
                     price.innerHTML = `${detail.price.toLocaleString()}&#8363;`
                     main_detail.appendChild(price)
                 }
@@ -167,6 +168,145 @@ const Detail = () => {
                 process.className = styles.process
                 process.disabled = true
                 count_down.appendChild(process)
+
+                const size_guide = document.createElement('div')
+                size_guide.className = styles.size_guide
+                size_guide.innerHTML = `
+                    <img src="../../img/size.png" width="21px"/>
+                    <h4>Size guide</h4>
+                `
+                main_detail.appendChild(size_guide)
+
+                const size_box = document.createElement('div')
+                size_box.className = styles.size_box
+                main_detail.appendChild(size_box)
+                const size_btn = document.createElement('button')
+                size_btn.className = styles.size_btn
+                size_btn.textContent = 'SIZE'
+                size_box.appendChild(size_btn)
+                const size_items = document.createElement('div')
+                size_items.className = styles.size_items
+                size_box.appendChild(size_items)
+                detail.sizes.forEach((item) => {
+                    const size = document.createElement('button')
+                    size.textContent = item
+                    size_items.appendChild(size)
+                    size.addEventListener('click', () => {
+                        size_items.childNodes.forEach((val) => {
+                            val.style.backgroundColor = 'white'
+                            val.style.color = 'grey'
+                        })
+                        size.style.backgroundColor = 'black'
+                        size.style.color = 'white'
+                    })
+                })
+                const detail_actions = document.createElement('div')
+                detail_actions.className = styles.detail_actions
+                main_detail.appendChild(detail_actions)
+                const quantity_box = document.createElement('div')
+                quantity_box.className = styles.quantity_box
+                detail_actions.appendChild(quantity_box)
+                const quantity_input = document.createElement('input')
+                quantity_input.value = 1
+                quantity_box.appendChild(quantity_input)
+                const quantity_btns = document.createElement('div')
+                quantity_btns.className = styles.quantity_btns
+                quantity_box.appendChild(quantity_btns)
+                const increase = document.createElement('button')
+                increase.innerHTML = `<span class="material-symbols-outlined">add</span>`
+                quantity_btns.appendChild(increase)
+                const decrease = document.createElement('button')
+                decrease.innerHTML = `<span class="material-symbols-outlined">remove</span>`
+                quantity_btns.appendChild(decrease)
+                const addToCart = document.createElement('button')
+                addToCart.textContent = 'ADD TO CART'
+                addToCart.className = styles.addToCart
+                detail_actions.appendChild(addToCart)
+            
+                const buy_now = document.createElement('button')
+                buy_now.className = styles.buy_now
+                buy_now.textContent = 'BUY IT NOW'
+                main_detail.appendChild(buy_now)
+                
+                const nav_tabs = document.querySelectorAll(`.${styles.nav_tab} > li`);
+                const first_nav_tab = document.querySelector(`.${styles.nav_tab} > li:first-child`)
+                first_nav_tab.setAttribute('id', styles.nav_tab_after)
+                nav_tabs.forEach((item) => {
+                    item.addEventListener('mouseenter', () => {
+                        item.setAttribute('id', styles.nav_tab_after)
+                    });
+
+                    item.addEventListener('mouseleave', () => {
+                        item.removeAttribute('id')
+                    });
+                });
+
+                const related_products_data = await getProductsByCategoryId(detail.cat_id)
+                const relate_slider = document.querySelector(`.${styles.relate_slider}`)
+                related_products_data.forEach((item) => {
+                    const relate_block = document.createElement('div')
+                    relate_block.className = styles.relate_block
+                    relate_slider.appendChild(relate_block)
+                    const relate_item = document.createElement('div')
+                    relate_item.addEventListener('mouseenter', () => {
+                       relate_item.childNodes[3].style.transform = 'translateY(-50px)'
+                    })
+                    relate_item.addEventListener('mouseleave', () => {
+                        relate_item.childNodes[3].style.transform = 'translateY(0)'
+                     })
+                    relate_item.className = styles.relate_item
+                    relate_block.appendChild(relate_item)
+                    const img_box = document.createElement('div')
+                    img_box.className = styles.img_box
+                    relate_item.appendChild(img_box)
+                    img_box.addEventListener('mouseenter', () => {
+                        img_box.childNodes[1].style.transform = 'translateY(0)'
+                    })
+                    img_box.addEventListener('mouseleave', () => {
+                        img_box.childNodes[1].style.transform = 'translateY(100px)'
+                    })
+                    const img = document.createElement('img')
+                    img.src = `../../img/${item.img_url[0]}`
+                    img.width = 315
+                    img_box.appendChild(img)
+                    const relate_funcs = document.createElement('div')
+                    relate_funcs.className = styles.relate_funcs
+                    img_box.appendChild(relate_funcs)
+                    const wishlist = document.createElement('button')
+                    wishlist.innerHTML = `<span class="material-symbols-outlined">favorite</span>`
+                    relate_funcs.appendChild(wishlist)
+                    const quick_view = document.createElement('button')
+                    quick_view.innerHTML = `<span class="material-symbols-outlined">search</span>`
+                    relate_funcs.appendChild(quick_view)
+                    const relate_title = document.createElement('h3')
+                    relate_title.className = styles.relate_title
+                    relate_title.textContent = item.name
+                    relate_item.appendChild(relate_title)
+                    if(item.promo_price){
+                        const price_box = document.createElement('div')
+                        price_box.className = styles.price_box
+                        relate_item.appendChild(price_box)
+                        const promo_price = document.createElement('h3')
+                        promo_price.innerHTML = `${item.promo_price.toLocaleString()}&#8363;`
+                        price_box.appendChild(promo_price)
+                        const price = document.createElement('h4')
+                        price.innerHTML = `<del>${item.price.toLocaleString()}&#8363;</del>`
+                        price_box.appendChild(price)
+                    }else{
+                        const price = document.createElement('h3')
+                        price.className = styles.price
+                        price.innerHTML = `${item.price.toLocaleString()}&#8363;`
+                        relate_item.appendChild(price)
+                    }
+
+                    const add_to_cart_box = document.createElement('div')
+                    add_to_cart_box.className = styles.add_to_cart_box
+                    relate_item.appendChild(add_to_cart_box)
+                    const add_to_cart = document.createElement('button')
+                    add_to_cart.className = styles.add_to_cart
+                    add_to_cart.textContent = 'ADD TO CART'
+                    add_to_cart_box.appendChild(add_to_cart)
+                })
             } catch (error) {
                 console.error("Error fetching detail:", error);
             }
@@ -266,6 +406,49 @@ const Detail = () => {
                         </div>
                     </div>
                 </div>
+
+                <div className={styles.tab_details}>
+                    <div className={styles.nav_tab}>
+                        <li><a>Description</a></li>
+                        <li><a>Additional Information</a></li>
+                        <li><a>Review</a></li>
+                    </div>
+                    <div className={styles.tab_container}>
+                        <div className={styles.desc_prod}>
+                            <div className={styles.row1}>
+                                <div className={styles.desc_col1}>
+                                    <img src="../../img/des1.webp"/>
+                                </div>
+                                <div className={styles.desc_col2}>
+                                    <h1>Calf-Length Dress In Airy, Textured Cotton Fabric With A Printed Pattern</h1>
+                                    <p>Sed hendrerit. Cras risus ipsum, faucibus ut, ullamcorper id, varius estibulum ante ipsum primis in faucibus</p>
+                                    <img src="../../img/des2.jpg"/>
+                                </div>
+                            </div>
+                            <div className={styles.desc_info}>
+                                <h3>PRODUCT DETAILS</h3>
+                                <p>Inspired by traditional blockprinting techniques in India, our own in-house design is the vibrant pattern that every closet needs. That's why we crafted our party standout tiered maxi dress in this royal blue-and-yellow print: It's lightweight, lined and will look great at all your most festive summer events.</p>
+                            </div>
+                            <div className={styles.img_block}>
+                                <img src="../../img/des3.webp"/>
+                            </div>
+                        </div>  
+                    </div>
+                </div>
+
+                <div className={styles.relate_products}>
+                    <div className={styles.relate_title}> 
+                        <h1>Relate Products</h1>
+                    </div>
+                    <div className={styles.relate_container}>
+                        <div className={styles.relate_slider}>
+
+                        </div>
+                    </div>
+                    <div className={styles.slider_btns}>
+
+                    </div>
+                </div>  
             </div>
         </>
     )
